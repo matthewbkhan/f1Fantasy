1bdf07d5902f941d1371851fb27723fcec0de47a
Matthew Khan <57141759+matthewbkhan@users.noreply.github.com>
2022-03-22 16:39:21 +0000

    big overhaul for Monte-Carlo positions

--- driverOpt.py
+++ driverOpt.py
@@ -6,127 +6,191 @@ driverData  = pd.read_csv('./driverData.csv')
 driverList  = driverData["DRIVER"].values
 driverTeam  = driverData["TEAM"].values
 driverCosts = driverData["COST"].values
-qualiPos    = driverData["QUALIPOS"].values
-racePos     = driverData["RACEPOS"].values
+prdQualiPos = driverData["QUALIPOS"].values
+prdRacePos  = driverData["RACEPOS"].values
 raceStdDev  = driverData["RACESTDDEV"].values
 pFinish     = driverData["PFINISH"].values
 pDNF        = 1.-pFinish
 
-""" Randomly distributes the positions with user specified std dev """
-randomPositions = [np.random.normal(rP,stdDev) for rP,stdDev in zip(racePos,raceStdDev)]
-sortedRandPos, sortedDrivers = zip(*sorted(zip(randomPositions,driverList)))
-sortedRandPos, sortedDrivers = np.array(sortedRandPos), np.array(sortedDrivers)
-sortedRandPos = np.arange(1,21,1)
-racePos       = np.array([sortedRandPos[np.where(sortedDrivers==dL)[0][0]] for dL in driverList])
+currentTeam     = ["MER","LEC","SAI","NOR","BOT","MAG"]
+currentBudget   = 100.
+lwrBdgtLimit    = 90.
+pointsThreshold = 220.
 
-currentTeam = ["MER","LEC","SAI","NOR","BOT","MAG"]
+# teamsList     = ["MER","RED","FER","MCL","ALP","REN","AST","ALF","WIL","HAA"]
+teamsList     = ["MER","RED","FER","ALP","REN","ALF","HAA"]
+teamCostsDict = {"MER":34.5,"RED":32.5,"FER":25.0,"MCL":18.5,"ALP":10.5,"REN":14.0,"AST":11.5,"ALF":8.0,"WIL":7.0,"HAA":6.0}
 
 """ Quali and race points, assumes that P1 gets fastest lap """
 qualiPoints  = np.array([10,  9, 8, 7, 6, 5,4,3,2,1,0,0,0,0,0,0,0,0,0,0])
 racePoints   = np.array([25+5,18,15,12,10,8,6,4,2,1,0,0,0,0,0,0,0,0,0,0])
 
-teamsList     = ["MER","RED","FER","MCL","ALP","REN","AST","ALF","WIL","HAA"]
-teamCostsDict = {"MER":34.5,"RED":32.5,"FER":25.0,"MCL":18.5,"ALP":10.5,"REN":14.0,"AST":11.5,"ALF":8.0,"WIL":7.0,"HAA":6.0}
+""" Add Gaussian errors to positions """
+def positionVariation(racePosition,raceStandardDeviation,driverNameList):
+    """ Randomly distributes the positions with user specified std dev """
+    randomPositions = [np.random.normal(rP,stdDev) for rP,stdDev in zip(racePosition,raceStandardDeviation)]
+    sortedRandPos, sortedDrivers = zip(*sorted(zip(randomPositions,driverNameList)))
+    sortedRandPos, sortedDrivers = np.array(sortedRandPos), np.array(sortedDrivers)
+    sortedRandPos = np.arange(1,21,1)
+    finalRacePos  = np.array([sortedRandPos[np.where(sortedDrivers==dL)[0][0]] for dL in driverNameList])
+    return finalRacePos
 
-""" Q1: 1 pnt, Q2: 2 pnts, Q3: 3 pnts """
-knockOutPnts = []
-for qP in qualiPos:
-    if(qP>15):
-        knP = 1
-    elif(qP<=15 and qP>10):
-        knP = 2
-    else:
-        knP = 3
-    knockOutPnts.append(knP)
+""" Qualifying session points """
+def knockOutSessionPoints(qualifyingPosition):
+    """ Q1: 1 pnt, Q2: 2 pnts, Q3: 3 pnts """
+    knockOutPnts = []
+    for qP in qualifyingPosition:
+        if(qP>15):
+            knP = 1
+        elif(qP<=15 and qP>10):
+            knP = 2
+        else:
+            knP = 3
+        knockOutPnts.append(knP)
+    return knockOutPnts
 
-""" +/-2 pnts per position change, max +/-10 """
-chngPosPnts = []
-for cPP in 2*np.array(qualiPos-racePos):
-    if(cPP<0):   chngPosPnts.append(np.max([cPP,-10]))
-    elif(cPP>=0):chngPosPnts.append(np.min([cPP, 10]))
+""" Change in position points """
+def changePositionPoints(qualifyingPosition,racePosition):
+    """ +/-2 pnts per position change, max +/-10 """
+    chngPosPnts = []
+    for cPP in 2*np.array(qualifyingPosition-racePosition):
+        if(cPP<0):   chngPosPnts.append(np.max([cPP,-10]))
+        elif(cPP>=0):chngPosPnts.append(np.min([cPP, 10]))
+    return chngPosPnts
 
-""" 3 points for beating teammate in race """
-teamMateRcePoints = []
-for dI,dT,rP in zip(range(len(driverList)),driverTeam,racePos):
-    teamMates = np.where(driverTeam==dT)[0]
-    teamMate  = np.delete(teamMates,np.where(teamMates==dI)[0])[0]
-    teamMtPos = racePos[teamMate]
-    if(rP<teamMtPos):
-        teamMateRcePoints.append(3)
-    else:
-        teamMateRcePoints.append(0)
+""" Points for beating teammate in race """
+def beatTeammateRace(driverTeamList,racePosition):
+    """ 3 points for beating teammate in race """
+    teamMateRcePoints = []
+    for dI,dT,rP in zip(range(len(driverTeamList)),driverTeamList,racePosition):
+        teamMates = np.where(driverTeamList==dT)[0]
+        teamMate  = np.delete(teamMates,np.where(teamMates==dI)[0])[0]
+        teamMtPos = racePos[teamMate]
+        if(rP<teamMtPos):
+            teamMateRcePoints.append(3)
+        else:
+            teamMateRcePoints.append(0)
+    return teamMateRcePoints
 
-""" 2 points for beating teammate in quali """
-teamMateQualiPoints = []
-for dI,dT,qP in zip(range(len(driverList)),driverTeam,qualiPos):
-    teamMates = np.where(driverTeam==dT)[0]
-    teamMate  = np.delete(teamMates,np.where(teamMates==dI)[0])[0]
-    teamMtPos = qualiPos[teamMate]
-    if(qP<teamMtPos):
-        teamMateQualiPoints.append(2)
-    else:
-        teamMateQualiPoints.append(0)
+""" Points for beating teammate in qualifying """
+def beatTeammateQuali(driverTeamList,qualifyingPosition):
+    """ 2 points for beating teammate in quali """
+    teamMateQualiPoints = []
+    for dI,dT,qP in zip(range(len(driverTeamList)),driverTeamList,qualifyingPosition):
+        teamMates = np.where(driverTeamList==dT)[0]
+        teamMate  = np.delete(teamMates,np.where(teamMates==dI)[0])[0]
+        teamMtPos = qualiPos[teamMate]
+        if(qP<teamMtPos):
+            teamMateQualiPoints.append(2)
+        else:
+            teamMateQualiPoints.append(0)
+    return teamMateQualiPoints
 
-""" Total driver points """
-driverPoints = qualiPoints[qualiPos-1] + knockOutPnts + teamMateQualiPoints + pFinish*(racePoints[racePos-1] + chngPosPnts + teamMateRcePoints + 1) + pDNF*(-10.)
+""" Calulate Total Points """
+def calcTotPoints(qualiPos,knockOutPnts,teamMateQualiPoints,racePos,chngPosPnts,teamMateRcePoints):
+    """ Total driver points """
+    driverPointsList = qualiPoints[qualiPos-1] + knockOutPnts + teamMateQualiPoints + pFinish*(racePoints[racePos-1] + chngPosPnts + teamMateRcePoints + 1) + pDNF*(-10.)
+    return driverPointsList
 
-""" Order driver in points """
-sortedPoints, sortedDrivers, sortedCosts = zip(*sorted(zip(driverPoints,driverList,driverCosts)))
-sortedPoints, sortedDrivers, sortedCosts = np.array(sortedPoints)[::-1], np.array(sortedDrivers)[::-1], np.array(sortedCosts)[::-1]
-print "DRVR, PNTS"
-for sD,sP,sC in zip(sortedDrivers,sortedPoints,sortedCosts):
-    if(sC<=20):
-        print "%s*, %i"%(sD,sP)
-    else:
-        print "%s,  %i"%(sD,sP)
-
-""" Order driver in value """
-driverValue = driverPoints/driverCosts
-sortedValue, sortedDrivers = zip(*sorted(zip(driverValue,driverList)))
-sortedValue, sortedDrivers = np.array(sortedValue)[::-1], np.array(sortedDrivers)[::-1]
-print "\n##########"
-print "DRV, PNTS/$"
-for sD,sV in zip(sortedDrivers,sortedValue):
-    print "%s, %.3f"%(sD,sV)
+""" Printing Stuff """
+def printDriverInfo(driverPoints,driverList,racePos,driverCosts):
+    """ Order driver in points """
+    sortedPoints, sortedDrivers, sortedRacePos, sortedCost = zip(*sorted(zip(driverPoints,driverList,racePos,driverCosts)))
+    sortedPoints, sortedDrivers, sortedRacePos, sortedCost = np.array(sortedPoints)[::-1], np.array(sortedDrivers)[::-1], np.array(sortedRacePos)[::-1], np.array(sortedCost)[::-1]
+    print "DRVR, POS, PNTS"
+    for sD,sP,sRP,sC in zip(sortedDrivers,sortedPoints,sortedRacePos,sortedCost):
+        if(sC<=20):
+            print "%s*, %i,   %i"%(sD,sRP,sP)
+        else:
+            print "%s,  %i,   %i"%(sD,sRP,sP)
+    """ Order driver in value """
+    driverValue = driverPoints/driverCosts
+    sortedValue, sortedDrivers = zip(*sorted(zip(driverValue,driverList)))
+    sortedValue, sortedDrivers = np.array(sortedValue)[::-1], np.array(sortedDrivers)[::-1]
+    print "\n##########"
+    print "DRV, PNTS/$"
+    for sD,sV in zip(sortedDrivers,sortedValue):
+        print "%s, %.3f"%(sD,sV)
+    return
 
 """ Main loop, it ain't pretty """
-maxPoints    = 0.
-indexRange   = np.arange(len(driverList))
-iterationInd = 0
-print "\n##########"
-for team in teamsList:
-    for i1,d1,c1,p1,t1 in zip(indexRange,driverList,driverCosts,driverPoints,driverTeam):
-        for i2,d2,c2,p2,t2 in zip(indexRange[i1+1:],driverList[i1+1:],driverCosts[i1+1:],driverPoints[i1+1:],driverTeam[i1+1:]):
-            for i3,d3,c3,p3,t3 in zip(indexRange[i2+1:],driverList[i2+1:],driverCosts[i2+1:],driverPoints[i2+1:],driverTeam[i2+1:]):
-                for i4,d4,c4,p4,t3 in zip(indexRange[i3+1:],driverList[i3+1:],driverCosts[i3+1:],driverPoints[i3+1:],driverTeam[i3+1:]):
-                    for i5,d5,c5,p5,t4 in zip(indexRange[i4+1:],driverList[i4+1:],driverCosts[i4+1:],driverPoints[i4+1:],driverTeam[i4+1:]):
-                        if(iterationInd%15500==0): print "%i%%"%(100.*iterationInd/155000.)
-                        iterationInd += 1
-                        totalCost = c1+c2+c3+c4+c5+teamCostsDict[team]
-                        if(totalCost>100.):continue
-                        teamPoints   = np.sum(driverPoints[np.where(driverTeam==team)])-3-2 #NB the -2 might be wrong for pFinish!=1
-                        lineUpCosts  = np.array([c1,c2,c3,c4,c5])
-                        driverLineUp = np.array([d1,d2,d3,d4,d5])
-                        lineUpPoints = np.array([p1,p2,p3,p4,p5])
-                        turboPoint   = np.max(lineUpPoints[lineUpCosts<=20.])
-                        """ Points reduction for switching """
-                        if(team in currentTeam):
-                            numDiff = 0
-                        else:
-                            numDiff = 1
-                        for dLU in driverLineUp:
-                            if(dLU not in currentTeam):
-                                numDiff += 1
-                        switchCost   = np.min([0,-10.*(numDiff-3)])
-                        totPoints    = np.sum(lineUpPoints)+teamPoints+turboPoint+switchCost
-                        if totPoints>maxPoints:
-                            bestDrivers = driverLineUp
-                            bestTeam    = team
-                            turboDrive  = driverLineUp[np.argmax(lineUpPoints*(lineUpCosts<=20.))]
-                            maxPoints   = totPoints
-                            bestCost    = totalCost
+comboPoints = np.zeros(21346) # NB these numbers will need changing if the threshold values are changed
+comboNames  = [[] for i in range(21346)]
+numberRanIt = 100
+np.random.seed(0)
+for randomIndex in range(numberRanIt):
+    print "Iteration Index %i: %i%% complete"%(randomIndex,100.*(float(randomIndex)/float(numberRanIt)))
+
+    # qualiPos            = positionVariation(prdQualiPos,raceStdDev,driverList)
+    racePos             = positionVariation(prdRacePos,raceStdDev,driverList)
+    qualiPos            = prdQualiPos
+    # racePos             = prdRacePos
+    knockOutPnts        = knockOutSessionPoints(qualiPos)
+    chngPosPnts         = changePositionPoints(qualiPos,racePos)
+    teamMateRcePoints   = beatTeammateRace(driverTeam,racePos)
+    teamMateQualiPoints = beatTeammateQuali(driverTeam,qualiPos)
+    driverPoints        = calcTotPoints(qualiPos,knockOutPnts,teamMateQualiPoints,racePos,chngPosPnts,teamMateRcePoints)
+    printDriverInfo(driverPoints,driverList,racePos,driverCosts)
+
+    maxPoints    = 0.
+    indexRange   = np.arange(len(driverList))
+    iterationInd = 0
+    numUnderCost = 0
+    # print "\n##########"
+    for team in teamsList:
+        for i1,d1,c1,p1,t1 in zip(indexRange,driverList,driverCosts,driverPoints,driverTeam):
+            for i2,d2,c2,p2,t2 in zip(indexRange[i1+1:],driverList[i1+1:],driverCosts[i1+1:],driverPoints[i1+1:],driverTeam[i1+1:]):
+                for i3,d3,c3,p3,t3 in zip(indexRange[i2+1:],driverList[i2+1:],driverCosts[i2+1:],driverPoints[i2+1:],driverTeam[i2+1:]):
+                    for i4,d4,c4,p4,t4 in zip(indexRange[i3+1:],driverList[i3+1:],driverCosts[i3+1:],driverPoints[i3+1:],driverTeam[i3+1:]):
+                        for i5,d5,c5,p5,t5 in zip(indexRange[i4+1:],driverList[i4+1:],driverCosts[i4+1:],driverPoints[i4+1:],driverTeam[i4+1:]):
+                            # if(iterationInd%15500==0): print "%i%%"%(100.*iterationInd/155000.)
+                            iterationInd += 1
+                            totalCost = c1+c2+c3+c4+c5+teamCostsDict[team]
+                            if(totalCost>currentBudget or totalCost<lwrBdgtLimit):continue
+                            teamPoints   = np.sum(driverPoints[np.where(driverTeam==team)])-3-2
+                            lineUpCosts  = np.array([c1,c2,c3,c4,c5])
+                            driverLineUp = np.array([d1,d2,d3,d4,d5])
+                            lineUpPoints = np.array([p1,p2,p3,p4,p5])
+                            turboPoint   = np.max(lineUpPoints[lineUpCosts<=20.])
+                            """ Points reduction for switching """
+                            if(team in currentTeam):
+                                numDiff = 0
+                            else:
+                                numDiff = 1
+                            for dLU in driverLineUp:
+                                if(dLU not in currentTeam):
+                                    numDiff += 1
+                            switchCost   = np.min([0,-10.*(numDiff-3)])
+                            totPoints    = np.sum(lineUpPoints)+teamPoints+turboPoint+switchCost
+                            comboPoints[numUnderCost] += totPoints
+                            if(randomIndex==0):
+                                comboNames[numUnderCost] = team+" "+d1+" "+d2+" "+d3+" "+d4+" "+d5
+                            if totPoints>maxPoints:
+                                bestDrivers = driverLineUp
+                                bestTeam    = team
+                                turboDrive  = driverLineUp[np.argmax(lineUpPoints*(lineUpCosts<=20.))]
+                                maxPoints   = totPoints
+                                bestCost    = totalCost
+                            numUnderCost += 1
+
+# sys.exit()
+import matplotlib.pyplot as plt
+comboPoints    = np.array(comboPoints)/numberRanIt
+comboNames     = np.array(comboNames)
+topComboPoints = comboPoints[(comboPoints>pointsThreshold)]
+topComboNames  = comboNames[(comboPoints>pointsThreshold)]
+topComboPoints, topComboNames = zip(*sorted(zip(topComboPoints, topComboNames)))
+for cP,cN in zip(topComboPoints[::-1],topComboNames[::-1]):
+    print cP,cN
+plt.figure(1,figsize=(16,9))
+plt.plot(topComboNames[::-1],topComboPoints[::-1])
+plt.ylabel("Mean Points")
+plt.xticks(rotation='vertical')
+plt.tight_layout()
+plt.show()
 
 """ Results! """
+print numUnderCost,iterationInd
 print "Drivers List: ",bestDrivers
 print "Constructors: ",bestTeam
 print "Turbo Driver: ",turboDrive
