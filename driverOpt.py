4c741e53c8254e482c336f914f6a4b5827ab42c1
Matthew Khan <57141759+matthewbkhan@users.noreply.github.com>
2022-03-28 11:41:47 +0100

    split single race and monte carlo, set up for streaks

--- driverOpt.py
+++ driverOpt.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import sys
 
+racesCompleted = ["BAHRAIN","SAUDI"]
+
 driverData  = pd.read_csv('./driverData.csv')
 driverList  = driverData["DRIVER"].values
 driverTeam  = driverData["TEAM"].values
@@ -12,18 +14,26 @@ raceStdDev  = driverData["RACESTDDEV"].values
 pFinish     = driverData["PFINISH"].values
 pDNF        = 1.-pFinish
 
+""" Driver Qualifying - driver qualifies in the Top 10 for 5 qualifying sessions in a row           = +5  pts
+    Driver Race - driver finishes in the Top 10 for 5 races in a row                                = +10 pts
+    Constructor Qualifying - both drivers qualify in the Top 10 for 3 qualifying sessions in a row  = +5  pts
+    Constructor Race - both drivers finish in the Top 10 for 3 races in a row                       = +10 pts """
+
+prevQualis  = np.array([driverData[raceName+"QPOS"].values for raceName in racesCompleted])
+prevRaces   = np.array([driverData[raceName+"RPOS"].values for raceName in racesCompleted])
+
 currentTeam     = ["MER","LEC","SAI","NOR","BOT","MAG"]
 currentBudget   = 100.
-lwrBdgtLimit    = 90.
-pointsThreshold = 220.
+lwrBdgtLimit    = 70.
+pointsThreshold = 200.
 
-# teamsList     = ["MER","RED","FER","MCL","ALP","REN","AST","ALF","WIL","HAA"]
-teamsList     = ["MER","RED","FER","ALP","REN","ALF","HAA"]
+teamsList     = ["MER","RED","FER","MCL","ALP","REN","AST","ALF","WIL","HAA"]
+# teamsList     = ["MER","RED","FER","ALP","REN","ALF","HAA"]
 teamCostsDict = {"MER":34.5,"RED":32.5,"FER":25.0,"MCL":18.5,"ALP":10.5,"REN":14.0,"AST":11.5,"ALF":8.0,"WIL":7.0,"HAA":6.0}
 
 """ Quali and race points, assumes that P1 gets fastest lap """
 qualiPoints  = np.array([10,  9, 8, 7, 6, 5,4,3,2,1,0,0,0,0,0,0,0,0,0,0])
-racePoints   = np.array([25+5,18,15,12,10,8,6,4,2,1,0,0,0,0,0,0,0,0,0,0])
+racePoints   = np.array([25,18+5,15,12,10,8,6,4,2,1,0,0,0,0,0,0,0,0,0,0])
 
 """ Add Gaussian errors to positions """
 def positionVariation(racePosition,raceStandardDeviation,driverNameList):
@@ -113,18 +123,10 @@ def printDriverInfo(driverPoints,driverList,racePos,driverCosts):
         print "%s, %.3f"%(sD,sV)
     return
 
-""" Main loop, it ain't pretty """
-comboPoints = np.zeros(21346) # NB these numbers will need changing if the threshold values are changed
-comboNames  = [[] for i in range(21346)]
-numberRanIt = 100
-np.random.seed(0)
-for randomIndex in range(numberRanIt):
-    print "Iteration Index %i: %i%% complete"%(randomIndex,100.*(float(randomIndex)/float(numberRanIt)))
-
-    # qualiPos            = positionVariation(prdQualiPos,raceStdDev,driverList)
-    racePos             = positionVariation(prdRacePos,raceStdDev,driverList)
-    qualiPos            = prdQualiPos
-    # racePos             = prdRacePos
+""" Single Race """
+if(1):
+    qualiPos            = prevQualis[-1]#prdQualiPos
+    racePos             = prevRaces[-1]#prdRacePos
     knockOutPnts        = knockOutSessionPoints(qualiPos)
     chngPosPnts         = changePositionPoints(qualiPos,racePos)
     teamMateRcePoints   = beatTeammateRace(driverTeam,racePos)
@@ -132,67 +134,178 @@ for randomIndex in range(numberRanIt):
     driverPoints        = calcTotPoints(qualiPos,knockOutPnts,teamMateQualiPoints,racePos,chngPosPnts,teamMateRcePoints)
     printDriverInfo(driverPoints,driverList,racePos,driverCosts)
 
+    comboPoints = []
+    comboNames  = []
+
     maxPoints    = 0.
     indexRange   = np.arange(len(driverList))
     iterationInd = 0
-    numUnderCost = 0
-    # print "\n##########"
     for team in teamsList:
         for i1,d1,c1,p1,t1 in zip(indexRange,driverList,driverCosts,driverPoints,driverTeam):
             for i2,d2,c2,p2,t2 in zip(indexRange[i1+1:],driverList[i1+1:],driverCosts[i1+1:],driverPoints[i1+1:],driverTeam[i1+1:]):
                 for i3,d3,c3,p3,t3 in zip(indexRange[i2+1:],driverList[i2+1:],driverCosts[i2+1:],driverPoints[i2+1:],driverTeam[i2+1:]):
                     for i4,d4,c4,p4,t4 in zip(indexRange[i3+1:],driverList[i3+1:],driverCosts[i3+1:],driverPoints[i3+1:],driverTeam[i3+1:]):
                         for i5,d5,c5,p5,t5 in zip(indexRange[i4+1:],driverList[i4+1:],driverCosts[i4+1:],driverPoints[i4+1:],driverTeam[i4+1:]):
-                            # if(iterationInd%15500==0): print "%i%%"%(100.*iterationInd/155000.)
+                            if(iterationInd%15500==0): print "%i%%"%(100.*iterationInd/155000.)
                             iterationInd += 1
                             totalCost = c1+c2+c3+c4+c5+teamCostsDict[team]
-                            if(totalCost>currentBudget or totalCost<lwrBdgtLimit):continue
+                            if(totalCost>currentBudget):continue
                             teamPoints   = np.sum(driverPoints[np.where(driverTeam==team)])-3-2
                             lineUpCosts  = np.array([c1,c2,c3,c4,c5])
                             driverLineUp = np.array([d1,d2,d3,d4,d5])
                             lineUpPoints = np.array([p1,p2,p3,p4,p5])
                             turboPoint   = np.max(lineUpPoints[lineUpCosts<=20.])
                             """ Points reduction for switching """
-                            if(team in currentTeam):
-                                numDiff = 0
+                            if(0):
+                                if(team in currentTeam):
+                                    numDiff = 0
+                                else:
+                                    numDiff = 1
+                                for dLU in driverLineUp:
+                                    if(dLU not in currentTeam):
+                                        numDiff += 1
                             else:
-                                numDiff = 1
-                            for dLU in driverLineUp:
-                                if(dLU not in currentTeam):
-                                    numDiff += 1
+                                numDiff = 0
                             switchCost   = np.min([0,-10.*(numDiff-3)])
                             totPoints    = np.sum(lineUpPoints)+teamPoints+turboPoint+switchCost
-                            comboPoints[numUnderCost] += totPoints
-                            if(randomIndex==0):
-                                comboNames[numUnderCost] = team+" "+d1+" "+d2+" "+d3+" "+d4+" "+d5
+                            if(totPoints>200.):
+                                comboPoints.append(totPoints)
+                                comboNames.append(team+" "+d1+" "+d2+" "+d3+" "+d4+" "+d5+" "+str(totalCost))
                             if totPoints>maxPoints:
                                 bestDrivers = driverLineUp
                                 bestTeam    = team
                                 turboDrive  = driverLineUp[np.argmax(lineUpPoints*(lineUpCosts<=20.))]
                                 maxPoints   = totPoints
                                 bestCost    = totalCost
-                            numUnderCost += 1
-
-# sys.exit()
-import matplotlib.pyplot as plt
-comboPoints    = np.array(comboPoints)/numberRanIt
-comboNames     = np.array(comboNames)
-topComboPoints = comboPoints[(comboPoints>pointsThreshold)]
-topComboNames  = comboNames[(comboPoints>pointsThreshold)]
-topComboPoints, topComboNames = zip(*sorted(zip(topComboPoints, topComboNames)))
-for cP,cN in zip(topComboPoints[::-1],topComboNames[::-1]):
-    print cP,cN
-plt.figure(1,figsize=(16,9))
-plt.plot(topComboNames[::-1],topComboPoints[::-1])
-plt.ylabel("Mean Points")
-plt.xticks(rotation='vertical')
-plt.tight_layout()
-plt.show()
 
 """ Results! """
-print numUnderCost,iterationInd
 print "Drivers List: ",bestDrivers
 print "Constructors: ",bestTeam
 print "Turbo Driver: ",turboDrive
 print "Prdcted Pnts: ",maxPoints
 print "Total Cost/$: ",bestCost
+if(1):
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    comboPoints    = np.array(comboPoints)
+    comboNames     = np.array(comboNames)
+    for pT in np.arange(200,300,5):
+        numAbove = np.sum((comboPoints>pT))
+        if(numAbove<=40):
+            pointsThreshold = pT
+            break
+    topComboPoints = comboPoints[(comboPoints>pointsThreshold)]
+    topComboNames  = comboNames[(comboPoints>pointsThreshold)]
+    topComboPoints, topComboNames = zip(*sorted(zip(topComboPoints, topComboNames)))
+    for cP,cN in zip(topComboPoints[::-1],topComboNames[::-1]):
+        print cP,cN
+    mpl.rcParams['font.size']=18
+    plt.figure(1,figsize=(16,9))
+    plt.plot(topComboNames[::-1],topComboPoints[::-1],marker="x",linestyle="--",mew=4,ms=10,color="tab:red")
+    plt.ylabel("Combination Points")
+    plt.xticks(rotation='vertical',fontsize=14)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("comboPoints.png")
+    plt.show()
+sys.exit()
+
+""" Monte Carlo Loop """
+if(0):
+    comboPoints = np.zeros(92892)#21346) # NB these numbers will need changing if the threshold values are changed
+    comboNames  = [[] for i in range(92892)]#21346)]
+    nmbrRndInt  = 50
+    totalDriverPoints = np.array([np.array([0 for x in range(20)]) for y in range(nmbrRndInt)])
+    # np.random.seed(0)
+    for randomIndex in range(nmbrRndInt):
+        print "Iteration Index %i: %i%% complete"%(randomIndex,100.*(float(randomIndex)/float(nmbrRndInt)))
+
+        """ Random """
+        # qualiPos            = positionVariation(prdQualiPos,raceStdDev,driverList)
+        racePos             = positionVariation(prdRacePos,raceStdDev,driverList)
+        """ Predicted """
+        qualiPos            = prdQualiPos
+        # racePos             = prdRacePos
+        knockOutPnts        = knockOutSessionPoints(qualiPos)
+        chngPosPnts         = changePositionPoints(qualiPos,racePos)
+        teamMateRcePoints   = beatTeammateRace(driverTeam,racePos)
+        teamMateQualiPoints = beatTeammateQuali(driverTeam,qualiPos)
+        driverPoints        = calcTotPoints(qualiPos,knockOutPnts,teamMateQualiPoints,racePos,chngPosPnts,teamMateRcePoints)
+        totalDriverPoints[randomIndex,:] = driverPoints[:]
+        # continue
+        # printDriverInfo(driverPoints,driverList,racePos,driverCosts)
+
+        maxPoints    = 0.
+        indexRange   = np.arange(len(driverList))
+        iterationInd = 0
+        numUnderCost = 0
+        # print "\n##########"
+        for team in teamsList:
+            for i1,d1,c1,p1,t1 in zip(indexRange,driverList,driverCosts,driverPoints,driverTeam):
+                for i2,d2,c2,p2,t2 in zip(indexRange[i1+1:],driverList[i1+1:],driverCosts[i1+1:],driverPoints[i1+1:],driverTeam[i1+1:]):
+                    for i3,d3,c3,p3,t3 in zip(indexRange[i2+1:],driverList[i2+1:],driverCosts[i2+1:],driverPoints[i2+1:],driverTeam[i2+1:]):
+                        for i4,d4,c4,p4,t4 in zip(indexRange[i3+1:],driverList[i3+1:],driverCosts[i3+1:],driverPoints[i3+1:],driverTeam[i3+1:]):
+                            for i5,d5,c5,p5,t5 in zip(indexRange[i4+1:],driverList[i4+1:],driverCosts[i4+1:],driverPoints[i4+1:],driverTeam[i4+1:]):
+                                # if(iterationInd%15500==0): print "%i%%"%(100.*iterationInd/155000.)
+                                iterationInd += 1
+                                totalCost = c1+c2+c3+c4+c5+teamCostsDict[team]
+                                if(totalCost>currentBudget or totalCost<lwrBdgtLimit):continue
+                                teamPoints   = np.sum(driverPoints[np.where(driverTeam==team)])-3-2
+                                lineUpCosts  = np.array([c1,c2,c3,c4,c5])
+                                driverLineUp = np.array([d1,d2,d3,d4,d5])
+                                lineUpPoints = np.array([p1,p2,p3,p4,p5])
+                                turboPoint   = np.max(lineUpPoints[lineUpCosts<=20.])
+                                """ Points reduction for switching """
+                                if(team in currentTeam):
+                                    numDiff = 0
+                                else:
+                                    numDiff = 1
+                                for dLU in driverLineUp:
+                                    if(dLU not in currentTeam):
+                                        numDiff += 1
+                                switchCost   = np.min([0,-10.*(numDiff-3)])
+                                totPoints    = np.sum(lineUpPoints)+teamPoints+turboPoint+switchCost
+                                comboPoints[numUnderCost] += totPoints
+                                if(randomIndex==0):
+                                    comboNames[numUnderCost] = team+" "+d1+" "+d2+" "+d3+" "+d4+" "+d5
+                                if totPoints>maxPoints:
+                                    bestDrivers = driverLineUp
+                                    bestTeam    = team
+                                    turboDrive  = driverLineUp[np.argmax(lineUpPoints*(lineUpCosts<=20.))]
+                                    maxPoints   = totPoints
+                                    bestCost    = totalCost
+                                numUnderCost += 1
+
+    # import matplotlib.pyplot as plt
+    # print totalDriverPoints
+    # driverPointsMean = np.mean(totalDriverPoints,axis=0)#/driverCosts
+    # driverPointsStd  = np.std(totalDriverPoints,axis=0)#/driverCosts
+    # driverPointsMean, driverNames, driverPointsStd = zip(*sorted(zip(driverPointsMean, driverList, driverPointsStd)))
+    # plt.errorbar(driverNames,driverPointsMean,yerr=driverPointsStd,linestyle="",marker="x",ms=10,mew=4)
+    # # plt.xticks(rotation='vertical')
+    # plt.show()
+    # print stdDev
+    # print mean
+    # sys.exit()
+    # print numUnderCost,iterationInd
+    # # sys.exit()
+    if(1):
+        import matplotlib.pyplot as plt
+        comboPoints    = np.array(comboPoints)/nmbrRndInt
+        comboNames     = np.array(comboNames)
+        for pT in np.arange(200,300,5):
+            numAbove = np.sum((comboPoints>pT))
+            if(numAbove<=40):
+                pointsThreshold = pT
+                break
+        topComboPoints = comboPoints[(comboPoints>pointsThreshold)]
+        topComboNames  = comboNames[(comboPoints>pointsThreshold)]
+        topComboPoints, topComboNames = zip(*sorted(zip(topComboPoints, topComboNames)))
+        for cP,cN in zip(topComboPoints[::-1],topComboNames[::-1]):
+            print cP,cN
+        plt.figure(1,figsize=(16,9))
+        plt.plot(topComboNames[::-1],topComboPoints[::-1])
+        plt.ylabel("Mean Points")
+        plt.xticks(rotation='vertical')
+        plt.tight_layout()
+        plt.show()
